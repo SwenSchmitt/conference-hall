ba284c4da5cdb25f0e4ed228d399736f34516662
FRIN Yvonnick <frin.yvonnick@gmail.com>
2019-08-01 17:27:05 +0200

    :sparkles: Remove a meetup

--- src/screens/organizer/meetup/form/meetupForm.jsx
+++ src/screens/organizer/meetup/form/meetupForm.jsx
@@ -2,18 +2,24 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Form } from 'react-final-form'
 
+import Button from 'components/button'
 import Field from 'components/form/field'
 import { input, textarea, SubmitButton } from 'components/form'
 import { required } from 'components/form/validators'
 
 import './meetupForm.css'
 
-const MeetupForm = ({ onSubmit, initialValues, submitting }) => (
+const MeetupForm = ({ onDelete, onSubmit, initialValues, submitting }) => (
   <Form onSubmit={onSubmit} initialValues={initialValues}>
     {({ handleSubmit, pristine }) => (
       <form className="meetup-form card">
         <Field name="name" label="Name" type="text" component={input} validate={required} />
         <Field name="description" label="Description" type="text" component={textarea} />
+        {initialValues && (
+          <Button error secondary onClick={onDelete}>
+            Delete Meetup
+          </Button>
+        )}
         <SubmitButton handleSubmit={handleSubmit} pristine={pristine} submitting={submitting}>
           {!initialValues ? 'Create meetup' : 'Save meetup'}
         </SubmitButton>
@@ -24,12 +30,14 @@ const MeetupForm = ({ onSubmit, initialValues, submitting }) => (
 
 MeetupForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
+  onDelete: PropTypes.func,
   initialValues: PropTypes.object,
   submitting: PropTypes.bool,
 }
 
 MeetupForm.defaultProps = {
   initialValues: undefined,
+  onDelete: () => {},
   submitting: false,
 }
 
